83e269dd259150d2176c3913dc124abc0dd3973e
shivam-pandey123-coder <skp526304@gmail.com>
2025-12-21 12:05:38 +0530

    cors setting

--- backend-Ecommerce/app.js
+++ backend-Ecommerce/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import formidable from "express-formidable";
 import path from "path";
+import cors from 'cors'
 
 const app = express();
 
@@ -9,6 +10,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",                     // Keep this for local testing
+    "https://ecommerce-sand-alpha.vercel.app"  // <--- PASTE IT HERE (Add https://)
+  ],
+  credentials: true
+}));
 // app.use(formidable());
 
 import userRouter from "./routes/user.routes.js";
