b1c983609bde6302f6c6b2a2194d1f27c5e6f04b
shivam-pandey123-coder <skp526304@gmail.com>
2026-01-03 12:24:18 +0530

    Update cookie settings for JWT in Token.js

--- backend-Ecommerce/utils/Token.js
+++ backend-Ecommerce/utils/Token.js
@@ -10,8 +10,8 @@ const generateToken = (res, userId) => {
   // set JWT as an jwt to HTTP-only
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV != 'development',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: process.env.NODE_ENV === 'development' ? 'strict' : 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000
   });
 
